be226bb741796808dc2156109a2ee1202bca4f6a
Amena Shahid <amenashahid@usf.edu>
2020-03-10 20:57:33 -0400

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace inclass10thMarch
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
